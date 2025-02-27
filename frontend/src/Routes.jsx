5614e2eb9274e25c597d106e5906faebb8584bd5
simran <simrank04u@gmail.com>
2025-02-27 21:42:28 +0530

    milestone_23

--- frontend/src/Routes.jsx
+++ frontend/src/Routes.jsx
@@ -8,7 +8,8 @@ import Cart from "./pages/cart.jsx";
 import ProductDetails from "./pages/productDetails.jsx";
 import Profile from "./pages/Profile.jsx"
 import CreateAddress from "./pages/createAddress.jsx";
+import SelectAddress from "./pages/selectAddress.jsx";
 
 
 
-export { SignupPage, LoginPage, Home, CreateProduct, MyProducts, Cart, ProductDetails,Profile,CreateAddress};
+export { SignupPage, LoginPage, Home, CreateProduct, MyProducts, Cart, ProductDetails,Profile,CreateAddress,SelectAddress };
